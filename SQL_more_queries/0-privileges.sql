c123921be68048d4f3f373c483542915008cae57
natiman <natiabnew@gmail.com>
2023-08-21 11:55:18 -0700

    fdfdsfsdfsdf

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- asasasasas
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
